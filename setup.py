cd2ed5d04c90670e7beae0dcbf5210a7776ec8f5
peisenha <peisenha@protonmail.com>
2020-05-10 19:50:08 +0200

    black formatting

--- setup.py
+++ setup.py
@@ -16,9 +16,6 @@ setup(
     author_email="linda.maokomatanda@gmail.com",
     packages=p,
     zip_safe=False,
-    package_data={
-        "utilities": [
-            ]
-    },
+    package_data={"utilities": []},
     include_package_data=True,
 )
