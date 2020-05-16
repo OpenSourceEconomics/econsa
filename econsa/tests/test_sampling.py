19d28b6f2d3d3103fc477324c9f48bae8648d589
peisenha <peisenha@protonmail.com>
2020-05-16 17:09:42 +0200

    added tests

---
+++ econsa/tests/test_sampling.py
@@ -0,0 +1,9 @@
+"""Tests for the sampling.
+
+This module contains all tests for th sampling setup.
+
+"""
+
+
+def test_mock():
+    pass
---
+++ econsa/tests/test_shapley.py
@@ -0,0 +1,9 @@
+"""Tests for the Shapley values.
+
+This module contains all tests for th Shapely values.
+
+"""
+
+
+def test_mock():
+    pass
