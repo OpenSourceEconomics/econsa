a726338d2fb435336ca31e8fae1e261322884e4d
peisenha <peisenha@protonmail.com>
2020-05-16 17:04:41 +0200

    initialize structure

---
+++ econsa/sampling.py
@@ -0,0 +1,9 @@
+"""Capabilities for sampling of random input parameters.
+
+This module contains all we need to sample random input parameters.
+
+"""
+
+
+def get_sample(*args, **kwargs):
+    return
