c9a4b5334aaade5b3ef1d7c97091cc1862758b7f
peisenha <peisenha@protonmail.com>
2020-05-25 17:38:41 +0200

    run figures now in script

---
+++ scripts/run_figures.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""Create figures
+
+This script supports our CI setup as it simply runs all files that create figures for our
+documentation.
+
+"""
+import subprocess as sp
+import glob
+import os
+
+if __name__ == "__main__":
+
+    os.chdir(os.environ["PROJECT_ROOT"] + "/docs/_static/codes")
+
+    [sp.check_call(f"python {fname}", shell=True) for fname in glob.glob("fig-*.py")]
