692743ff208c81ffd85139063fc8f0da3f56a1e3
loikein <wanleiqiong@gmail.com>
2020-07-07 00:33:31 +0200

    rollback & debug

--- econsa/tests/wrapper_r.py
+++ econsa/tests/wrapper_r.py
@@ -8,13 +8,11 @@ import rpy2.robjects.packages as rpackages
 from rpy2 import robjects
 from rpy2.robjects import numpy2ri
 
-r_package_utils = rpackages.importr("utils")
-r_package_utils.chooseCRANmirror(ind=1)
-r_package_utils.install_packages("condMVNorm")
 r_package_cond_mvnorm = rpackages.importr("condMVNorm")
 
 
 def r_cond_mvn(mean, sigma, dependent_ind, given_ind, given_value):
+    """The original function for `cond_mvn`."""
     numpy2ri.activate()
     r_mean = robjects.FloatVector(mean)
     n = sigma.shape[0]
@@ -22,12 +20,11 @@ def r_cond_mvn(mean, sigma, dependent_ind, given_ind, given_value):
     r_dependent_ind = robjects.IntVector([x + 1 for x in dependent_ind])
     r_given_ind = robjects.IntVector([x + 1 for x in given_ind])
     r_given_value = robjects.IntVector(given_value)
+    numpy2ri.deactivate()
 
     args = (r_mean, r_sigma, r_dependent_ind, r_given_ind, r_given_value)
     r_cond_mean, r_cond_cov = r_package_cond_mvnorm.condMVN(*args)
 
     r_cond_mean, r_cond_cov = np.array(r_cond_mean), np.array(r_cond_cov)
-
-    numpy2ri.deactivate()
 
     return r_cond_mean, r_cond_cov
