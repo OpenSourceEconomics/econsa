6a2fb2be9b72f97bc69a99ff613432cdf45d8d24
loikein <wanleiqiong@gmail.com>
2020-07-23 15:45:24 +0200

    remove extra dimension for 1d case

--- econsa/tests/test_copula.py
+++ econsa/tests/test_copula.py
@@ -18,6 +18,7 @@ def get_strategies(name):
     full = list(range(0, dim))
     given_ind = full[:]
 
+    # dependent_n = 1
     dependent_n = np.random.randint(low=1, high=dim)
     dependent_ind = np.random.choice(full, replace=False, size=dependent_n)
 
@@ -48,7 +49,6 @@ def get_strategies(name):
         given_value_u = [
             distribution[ind].cdf(given_value[i]) for i, ind in enumerate(given_ind)
         ]
-
         strategy_gc = (cov, dependent_ind, given_ind, given_value_u, distribution)
         strategy_cn = (means, cov, dependent_ind, given_ind, given_value)
         strategy = (strategy_gc, strategy_cn)
@@ -71,9 +71,10 @@ def test_cond_gaussian_copula():
     cov, dependent_ind, given_ind, given_value_u, distribution = args_gc
     dim = len(dependent_ind)
 
+    np.random.seed(123)
     condi_value_u = cond_gaussian_copula(cov, dependent_ind, given_ind, given_value_u)
     gc_value = [distribution[int(ind)].inv(condi_value_u) for ind in dependent_ind]
-    gc_value = np.atleast_1d(gc_value).reshape(dim, dim)
+    gc_value = np.squeeze(np.array(gc_value).reshape(dim, dim))
 
     np.random.seed(123)
     cond_mean, cond_cov = cond_mvn(*args_cn)
