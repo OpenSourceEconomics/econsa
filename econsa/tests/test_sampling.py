8a7cb45563001eaf4a09cda5ffbed8c483bdcf41
Leiqiong <wanleiqiong@gmail.com>
2020-12-07 13:31:43 +0100

    Clean up test_sampling (#79)
    * edit test_sampling
    * more docstrings

--- econsa/tests/test_sampling.py
+++ econsa/tests/test_sampling.py
@@ -1,7 +1,6 @@
 """Tests for the sampling.
 
 This module contains all tests for the sampling setup.
-
 """
 import numpy as np
 import pytest
@@ -24,14 +23,23 @@ def get_strategies(name):
         cov = cov @ cov.T
         given_ind = [x for x in range(0, n) if x not in dependent_ind]
         given_value = rng.uniform(low=-2, high=2, size=len(given_ind))
-    elif name == "cond_mvn_exception_given":
+    elif name == "cond_mvn_given_valid":
         cov = rng.standard_normal(size=(n, n))
         cov = cov @ cov.T
-        given_ind = [x for x in range(0, n) if x not in dependent_ind] if n % 3 == 0 else None
+        given_ind = None
         given_value = rng.uniform(low=-2, high=2, size=n - dependent_n + 1) if n % 2 == 0 else None
-    elif name == "test_cond_mvn_exception_cov":
-        cov = rng.standard_normal(size=(n, n)) if n % 3 == 0 else np.diagflat([-1] * n)
-        cov = cov @ cov.T if n % 2 == 0 else cov
+    elif name == "cond_mvn_given_invalid":
+        cov = rng.standard_normal(size=(n, n))
+        cov = cov @ cov.T
+        given_ind = [x for x in range(0, n) if x not in dependent_ind]
+        given_value = rng.uniform(low=-2, high=2, size=n - dependent_n + 1) if n % 2 == 0 else None
+    elif name == "cond_mvn_cov_negative":
+        cov = np.diagflat([-1] * n)
+        cov = cov
+        given_ind = [x for x in range(0, n) if x not in dependent_ind]
+        given_value = rng.uniform(low=-2, high=2, size=len(given_ind))
+    elif name == "cond_mvn_cov_asymmetric":
+        cov = rng.standard_normal(size=(n, n))
         given_ind = [x for x in range(0, n) if x not in dependent_ind]
         given_value = rng.uniform(low=-2, high=2, size=len(given_ind))
     else:
@@ -53,40 +61,29 @@ def test_cond_mvn():
     np.testing.assert_allclose(cond_cov, r_cond_cov)
 
 
-def test_cond_mvn_exception_given():
-    """Test cond_mvn raises exceptions when invalid `given_ind` or `given_value` is passed."""
-    mean, cov, dependent_ind, given_ind, given_value = get_strategies(
-        "cond_mvn_exception_given",
-    )
-
-    n = cov.shape[0]
-    if n % 3 != 0:
-        # Valid case: only `given_ind` is empty or both `given_ind` and `given_value` are empty
-        cond_mvn(mean, cov, dependent_ind, given_ind, given_value)
-    else:
-        # `given_value` is empty or does not align with `given_ind`
-        with pytest.raises(ValueError) as e:
-            cond_mvn(mean, cov, dependent_ind, given_ind, given_value)
-        assert "lengths of given_value and given_ind must be the same" in str(e.value)
+def test_cond_mvn_given_valid():
+    """Test valid case: only `given_ind` is empty
+    or both `given_ind` and `given_value` are empty.
+    """
+    cond_mvn(*get_strategies("cond_mvn_given_valid"))
 
 
-def test_cond_mvn_exception_cov():
-    """Test cond_mvn raises exceptions when invalid `cov` is passed."""
-    mean, cov, dependent_ind, given_ind, given_value = get_strategies(
-        "test_cond_mvn_exception_cov",
-    )
+def test_cond_mvn_given_invalid():
+    """Test invalid case: `given_value` is empty or does not align with `given_ind`."""
+    with pytest.raises(ValueError) as e:
+        cond_mvn(*get_strategies("cond_mvn_given_invalid"))
+    assert "lengths of given_value and given_ind must be the same" in str(e.value)
 
-    n = cov.shape[0]
 
-    if n % 3 != 0 and n % 2 != 0:
-        # `cov` is negative definite matrix
-        with pytest.raises(ValueError) as e:
-            cond_mvn(mean, cov, dependent_ind, given_ind, given_value)
-        assert "cov is not positive-definite" in str(e.value)
-    elif n % 2 != 0:
-        # `cov` is not symmetric
-        with pytest.raises(ValueError) as e:
-            cond_mvn(mean, cov, dependent_ind, given_ind, given_value)
-        assert "cov is not a symmetric matrix" in str(e.value)
-    else:
-        cond_mvn(mean, cov, dependent_ind, given_ind, given_value)
+def test_cond_mvn_cov_negative():
+    """Test invalid case: `cov` is not positive definite."""
+    with pytest.raises(ValueError) as e:
+        cond_mvn(*get_strategies("cond_mvn_cov_negative"))
+    assert "cov is not positive-definite" in str(e.value)
+
+
+def test_cond_mvn_cov_asymmetric():
+    """Test invalid case: `cov` is not symmetric."""
+    with pytest.raises(ValueError) as e:
+        cond_mvn(*get_strategies("cond_mvn_cov_asymmetric"))
+    assert "cov is not a symmetric matrix" in str(e.value)
