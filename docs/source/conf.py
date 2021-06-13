66a3a69f254f34b631f05c60c9e91e5344047ffb
bhmueller <s6bemuel@uni-bonn.de>
2021-06-14 01:55:41 +0200

    Change other files as well.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -142,9 +142,7 @@ nbsphinx_prolog = r"""
 {% set docname = 'docs/source/' + env.doc2path(env.docname, base=None) %}
 .. |binder| image:: https://mybinder.org/badge_logo.svg
      :target: https://mybinder.org/v2/gh/OpenSourceEconomics/econsa/master?filepath={{ docname|e }}
-
 .. only:: html
-
     .. nbinfo::
         Download the notebook :download:`here <https://nbviewer.jupyter.org/github/OpenSourceEconomics/econsa/blob/master/{{ docname }}>`!
         Interactive online version: |binder|
@@ -229,3 +227,6 @@ epub_exclude_files = ["search.html"]
 
 # Use new CDN path of MathJax
 mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+
+# Set bib file for sphinxcontrib-bibtex
+bibtex_bibfiles = ["bibliography.bib"]
